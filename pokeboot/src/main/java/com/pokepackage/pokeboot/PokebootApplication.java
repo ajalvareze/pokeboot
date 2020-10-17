a87a2035c0ef119808b87ecb4131a05e793b3bfb
ajalvareze <ajalvareze@gmail.com>
2020-10-16 22:16:38 -0500

    Inicializar repositorio

---
+++ pokeboot/src/main/java/com/pokepackage/pokeboot/PokebootApplication.java
@@ -0,0 +1,13 @@
+package com.pokepackage.pokeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebootApplication.class, args);
+	}
+
+}
---
+++ pokeboot/src/test/java/com/pokepackage/pokeboot/PokebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokepackage.pokeboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
